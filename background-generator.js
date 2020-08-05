0c2b728160c25e20cf92e11c604d3c5f6bb0d5e0
TejasvGoel <68171457+TejasvGoel@users.noreply.github.com>
2020-08-04 20:11:06 -0400

    Add files via upload

---
+++ background-generator.js
@@ -0,0 +1,16 @@
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var css = document.querySelector("h3");
+var body = document.getElementById("main");
+function colorChange() {
+    body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value} )`;
+    css.textContent = body.style.background + ";" ;
+}
+var colr = () => {
+    body.style.background = `linear-gradient(to right, ${color1.value}, ${color2.value} )`;
+    css.textContent = body.style.background + ";" ;
+}
+
+color1.addEventListener("input", colorChange);
+color2.addEventListener("input", colr);
+
